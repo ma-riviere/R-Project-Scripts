7372c7f1b8d55f8249b41d472c4b5e7e0f6b0387
Marc-Aurèle RIVIERE <36962105+ma-riviere@users.noreply.github.com>
2022-06-30 15:39:33 +0200

    Update theme.R

--- theme.R
+++ theme.R
@@ -20,7 +20,7 @@ strip_color <- "#adb5bd"
 
 options(
   ggplot2.discrete.colour = \() scale_color_viridis_d(),
-  ggplot2.discrete.fill = \() scale_color_viridis_d(),
+  ggplot2.discrete.fill = \() scale_fill_viridis_d(),
   ggplot2.continuous.colour = \() scale_color_viridis_c(),
   ggplot2.continuous.fill = \() scale_fill_viridis_c(),
   ggplot2.binned.colour = \() scale_color_viridis_b(),
