237466b836b3a00498b44f1237cbc3248451fc2e
Marc-Aurèle RIVIERE <36962105+ma-riviere@users.noreply.github.com>
2022-07-07 19:41:34 +0200

    Update setup.R

--- setup.R
+++ setup.R
@@ -21,7 +21,7 @@ if (!startsWith(.libPaths()[1], here::here())) {
   v <- paste0("R-", version$major, ".", strsplit(version$minor, ".", fixed = TRUE)[[1]][1])
   dir <- ifelse(Sys.info()[["sysname"]] == "Windows", "x86_64-w64-mingw32", "x86_64-pc-linux-gnu")
   path <- here::here("renv", "library", v, dir)
-  dir.create(path, recursive = TRUE)
+  if(!dir.exists(path)) dir.create(path, recursive = TRUE)
   .libPaths(path)
 }
 
